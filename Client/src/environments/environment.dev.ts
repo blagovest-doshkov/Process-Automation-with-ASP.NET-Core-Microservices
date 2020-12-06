694a1f0a25727ff08c7e1276e437795b19889ce3
blagovest <blagovest_doshkov@abv.bg>
2020-12-06 22:57:04 +0200

    Added Development configuration for the Client (Endpoints of the services)

--- Client/src/environments/environment.dev.ts
+++ Client/src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  identityApiUrl: 'http://104.154.171.17:5001/',
-  dealersApiUrl: 'http://34.69.197.123:5002/',
-  statisticsApiUrl: 'http://34.122.66.49:5003/',
-  notificationsUrl: 'http://notenoughcpus:5004/'
+  identityApiUrl: 'http://104.154.60.94:5001 /',
+  dealersApiUrl: 'http://34.121.221.14:5002/',
+  statisticsApiUrl: 'http://104.155.152.135:5003/',
+  notificationsUrl: 'http://notenoughloadbalancers:5004/'
 };
